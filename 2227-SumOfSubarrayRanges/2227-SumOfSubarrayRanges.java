cf8660e8986e3ab69218db63606f51b2ba1b3a3a
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2026-07-26 22:50:02 +0530

    Create 2227-SumOfSubarrayRanges

---
+++ 2227-SumOfSubarrayRanges/2227-SumOfSubarrayRanges.java
@@ -0,0 +1,19 @@
+// Last updated: 7/26/2026, 10:50:01 PM
+class Solution {
+    public long subArrayRanges(int[] nums) {
+        long sum = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+
+            for(int j = i; j < nums.length; j++){
+                min = Math.min(min, nums[j]);
+                max = Math.max(max, nums[j]);
+
+                sum += (max - min);   
+            }
+        }
+        return sum;
+    }
+}
