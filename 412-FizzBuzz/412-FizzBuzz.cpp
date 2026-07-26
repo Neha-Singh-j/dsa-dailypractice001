377724ac8c5b6d9c24f2d5e733d442feacff2a30
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2026-07-26 22:58:33 +0530

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.cpp
@@ -0,0 +1,24 @@
+// Last updated: 7/26/2026, 10:58:32 PM
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> arr;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                arr.push_back("FizzBuzz");
+            } 
+            else if (i % 3 == 0) {
+                arr.push_back("Fizz");
+            } 
+            else if (i % 5 == 0) {
+                arr.push_back("Buzz");
+            } 
+            else {
+                arr.push_back(to_string(i));
+            }
+        }
+
+        return arr;
+    }
+};
