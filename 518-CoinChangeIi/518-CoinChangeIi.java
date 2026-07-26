7b6315fa131e6891ad016906029f549a406f8d23
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2026-07-26 22:57:41 +0530

    Create 518-CoinChangeIi

---
+++ 518-CoinChangeIi/518-CoinChangeIi.java
@@ -0,0 +1,14 @@
+// Last updated: 7/26/2026, 10:57:39 PM
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp=new int[amount+1];
+        dp[0]=1;
+        int n=coins.length;
+        for(int i=0;i<n;i++){
+            for(int j=coins[i];j<=amount;j++){
+                dp[j]+=dp[j-coins[i]];
+            }
+        }
+        return dp[amount];
+    }
+}
