8047e844f07dde0f356ed5e68d91dc0a9915e586
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2026-07-26 22:55:27 +0530

    Create 890-LemonadeChange

---
+++ 890-LemonadeChange/890-LemonadeChange.java
@@ -0,0 +1,26 @@
+// Last updated: 7/26/2026, 10:55:26 PM
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+      int n=bills.length;
+    //    only possible coins to retrun in change
+       int five_dollar=0,ten_dollar=0;
+        for(int i=0;i<n;i++){
+             if(bills[i]==5){
+                five_dollar++;
+            }else if(bills[i]==10 && five_dollar>0){
+                five_dollar--;
+                ten_dollar++;
+            }else {
+                 if(five_dollar>0 && ten_dollar>0){
+                    five_dollar--;
+                    ten_dollar--;
+                }else if(five_dollar>2){
+                    five_dollar-=3;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
