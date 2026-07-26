335c015c02787510c9dc7a69deed4abc615bd924
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2026-07-26 22:56:10 +0530

    Create 720-LongestWordInDictionary

---
+++ 720-LongestWordInDictionary/720-LongestWordInDictionary.java
@@ -0,0 +1,18 @@
+// Last updated: 7/26/2026, 10:56:08 PM
+class Solution {
+    public String longestWord(String[] words) {
+        Arrays.sort(words); 
+        HashSet<String> set = new HashSet<>();
+        String ans = "";
+        for (String word : words) {
+            if (word.length() == 1 || set.contains(word.substring(0, word.length() - 1))) {
+                set.add(word);
+                if (word.length() > ans.length()) {
+                    ans = word;
+                }
+            }
+        }
+        return ans;
+
+    }
+}
