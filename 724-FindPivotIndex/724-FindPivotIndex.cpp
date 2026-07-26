588a10c7087dd8c4abb2da369a824572a8b77aa6
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2026-07-26 22:56:12 +0530

    Create 724-FindPivotIndex

---
+++ 724-FindPivotIndex/724-FindPivotIndex.cpp
@@ -0,0 +1,24 @@
+// Last updated: 7/26/2026, 10:56:10 PM
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> left(n), right(n);
+
+        right[n - 1] = 0;
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = nums[i + 1] + right[i + 1];
+        }
+
+        left[0] = 0;
+        for (int i = 1; i < n; i++) {
+            left[i] = nums[i - 1] + left[i - 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (left[i] == right[i]) return i;
+        }
+
+        return -1;
+    }
+};
