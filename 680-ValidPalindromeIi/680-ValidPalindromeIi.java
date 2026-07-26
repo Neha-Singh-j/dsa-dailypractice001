48b35fee02d7d9feaa49a021613ceb49bc0428a1
Neha Singh <165409678+Neha-Singh-j@users.noreply.github.com>
2026-07-26 22:56:24 +0530

    Create 680-ValidPalindromeIi

---
+++ 680-ValidPalindromeIi/680-ValidPalindromeIi.java
@@ -0,0 +1,28 @@
+// Last updated: 7/26/2026, 10:56:23 PM
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            if (s.charAt(i) == s.charAt(j)) {
+                i++;
+                j--;
+            } else {
+                return check(s, i + 1, j) || check(s, i, j - 1);
+            }
+        }
+
+        return true;
+    }
+
+    public boolean check(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
